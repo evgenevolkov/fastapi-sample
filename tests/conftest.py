401014ae8f56bdd7cf3d836b936387e800b5112c
Evgene V <evgenevolkov@users.noreply.github.com>
2024-09-26 16:07:17 +0200

    fix convert timeout envvar to int

--- tests/conftest.py
+++ tests/conftest.py
@@ -102,7 +102,7 @@ def get_token(user_id):
                     data={"user_id": user_id},
                     secret_key=config('SECRET_KEY'),
                     algorithm=config('ALGORITHM'),
-                    token_expire_minutes=config('TOKEN_EXPIRE_MINUTES')
+                    token_expire_minutes=int(config('TOKEN_EXPIRE_MINUTES'))
     )
     return auth_data['token']
 
